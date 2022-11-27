c985e8e53944ddec0acd66b45f32fbaa9db5e68b
Mohib <mohibul.khan@cleartrip.com>
2022-11-27 13:22:38 +0530

    added ex-6.2 spiral order matrix

---
+++ Week-6/ex-6.2/index.js
@@ -0,0 +1,40 @@
+function spiralOrderMatrix(matrix) {
+  let result = [];
+  let left = 0;
+  let right = matrix[0].length - 1;
+  let top = 0;
+  let bottom = matrix.length - 1;
+  let direction = 'right';
+
+  while (left <= right && top <= bottom) {
+    if (direction === 'right') {
+      for (let i = left; i <= right; i++) {
+        result.push(matrix[top][i]);
+      }
+      top++;
+      direction = 'down';
+    } else if (direction === 'down') {
+      for (let i = top; i <= bottom; i++) {
+        result.push(matrix[i][right]);
+      }
+      right--;
+      direction = 'left';
+    } else if (direction === 'left') {
+      for (let i = right; i >= left; i--) {
+        result.push(matrix[bottom][i]);
+      }
+      bottom--;
+      direction = 'up';
+    } else if (direction === 'up') {
+      for (let i = bottom; i >= top; i--) {
+        result.push(matrix[i][left]);
+      }
+      left++;
+      direction = 'right';
+    }
+  }
+  return result;
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
