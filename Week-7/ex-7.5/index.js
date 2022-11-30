ba701834bb7cc6b7fe155b13fe820f8e444248e5
Mohib <mohibul.khan@cleartrip.com>
2022-12-01 00:12:20 +0530

    added ex-7.5 next greater element

---
+++ Week-7/ex-7.5/index.js
@@ -0,0 +1,20 @@
+import Stack from '../ex-7.4';
+
+function nextGreaterElement(arr) {
+  const stack = new Stack();
+  const len = arr.length;
+  const result = [];
+
+  for (let i = len - 1; i >= 0; i--) {
+    while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+      stack.pop();
+    }
+    if (stack.isEmpty()) {
+      result[i] = -1;
+    } else {
+      result[i] = stack.peek();
+    }
+    stack.push(arr[i]);
+  }
+  return result.join(' ');
+}
