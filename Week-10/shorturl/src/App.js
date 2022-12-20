c48103ca4016d5dd5be7c559709975c49ff92c8a
Mohib <mohibul.khan@cleartrip.com>
2022-12-20 17:29:57 +0530

    added link input, axios

--- Week-10/shorturl/src/App.js
+++ Week-10/shorturl/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import LinkInput from './components/link-input';
 
 function App() {
+  // fetch(
+  //   'https://api.shrtco.de/v2/shorten?url=https://github.com/pesto-students/p6-gouthamarajan-mohibkay/pull/9'
+  // ).then((data) => console.log(data.json()));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>shorturl</h1>
       </header>
+
+      <main>
+        <LinkInput />
+      </main>
     </div>
   );
 }
---
+++ Week-10/shorturl/src/components/link-input/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import axios from 'axios';
+
+const LinkInput = () => {
+  const shortenLink = async (inputValue) => {
+    const { data } = await axios.get(
+      `https://api.shrtco.de/v2/shorten?url=${inputValue}`
+    );
+    console.log(data);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const inputValue = e.target[0].value;
+
+    if (inputValue) {
+      shortenLink(inputValue);
+    } else {
+      alert('URL can not be blank');
+    }
+  };
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <input type='url' placeholder='Shorten a link here...' />
+        <button type='submit'>Shorten It!</button>
+      </form>
+    </section>
+  );
+};
+
+export default LinkInput;
